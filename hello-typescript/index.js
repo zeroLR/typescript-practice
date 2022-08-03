11166763ee962eafd379ae998ffeef2ecfce8249
ceyang <ceyanglab@gmail.com>
2022-08-03 15:36:45 +0800

    initial commit

---
+++ hello-typescript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const message = "Hello World";
+function say(somthing) {
+    console.log(somthing);
+}
+say(message);
+console.log(message.touppercase());
---
+++ hello-typescript/index.ts
@@ -0,0 +1,8 @@
+const message = "Hello World";
+
+function say(somthing: string): void {
+  console.log(somthing);
+}
+
+say(message);
+console.log(message.touppercase());
